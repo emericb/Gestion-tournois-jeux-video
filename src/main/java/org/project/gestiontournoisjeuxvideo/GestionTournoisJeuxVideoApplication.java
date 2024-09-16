5792b2e343ca2ea3c34d874dda2f3abdbb70505c
Emeric Bous <38536419+emericb@users.noreply.github.com>
2024-09-16 09:46:16 +0200

    init
    init project

---
+++ src/main/java/org/project/gestiontournoisjeuxvideo/GestionTournoisJeuxVideoApplication.java
@@ -0,0 +1,13 @@
+package org.project.gestiontournoisjeuxvideo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionTournoisJeuxVideoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestionTournoisJeuxVideoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/project/gestiontournoisjeuxvideo/GestionTournoisJeuxVideoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.project.gestiontournoisjeuxvideo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionTournoisJeuxVideoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
