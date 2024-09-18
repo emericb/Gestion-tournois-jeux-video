7201e8ff94a91894072dac12a11d858d0b1779d2
Marie-Adele <136612477+Marie-Adele@users.noreply.github.com>
2024-09-18 09:26:52 +0200

    dev_ft
    Maj front, css

---
+++ src/main/java/org/project/gestiontournoisjeuxvideo/controller/ContactController.java
@@ -0,0 +1,14 @@
+package org.project.gestiontournoisjeuxvideo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+public class ContactController {
+
+    @RequestMapping("/contact")
+    public String contact() {
+        return "contact";
+    }
+}
